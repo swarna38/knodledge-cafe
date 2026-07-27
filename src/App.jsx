dcf79f57eee809b9d25c4cdc47deee7ee33a9705
swarna38 <swarna.sutradhar.info@gamil.com>
2026-07-27 12:43:16 +0600

    basic setup done with connect tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Header from './componenets/header/Header'
+
+function App() {
+  
+
+  return (
+    <>
+       <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/componenets/header/Header.jsx
@@ -0,0 +1,11 @@
+ 
+
+const Header = () => {
+    return (
+       <div className='justify-items-center p-4'>
+          <h3 className='text-black text-4xl font-bold'>knowledge cafe</h3>
+       </div>
+    );
+};
+
+export default Header;
