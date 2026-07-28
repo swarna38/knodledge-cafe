9d201447a57af933875562fbce936c35b2f49657
swarna38 <swarna.sutradhar.info@gamil.com>
2026-07-28 12:08:54 +0600

    heder profile pic done

--- src/componenets/header/Header.jsx
+++ src/componenets/header/Header.jsx
@@ -1,9 +1,10 @@
  
-
+import profile from '../../assets/img/profile.png'
 const Header = () => {
     return (
        <div className='justify-items-center p-4'>
           <h3 className='text-black text-4xl font-bold'>knowledge cafe</h3>
+          <img src={profile} alt="" />
        </div>
     );
 };
